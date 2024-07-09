45a7411c9b9af219992dc7c0eb45f8d988fd3d0a
Dan Barr <dan@dbarr.com>
2024-07-09 18:04:12 -0400

    Add support for trusted launch and confidential VMs

--- main.tf
+++ main.tf
@@ -24,13 +24,16 @@ resource "azurerm_shared_image_gallery" "packer" {
 }
 
 resource "azurerm_shared_image" "packer" {
-  for_each            = { for image in var.image_definitions : image.name => image }
-  name                = each.value.name
-  gallery_name        = azurerm_shared_image_gallery.packer.name
-  resource_group_name = data.azurerm_resource_group.packer.name
-  location            = data.azurerm_resource_group.packer.location
-  os_type             = each.value.os_type
-  hyper_v_generation  = each.value.generation
+  for_each = { for image in var.image_definitions : image.name => image }
+
+  name                    = each.value.name
+  gallery_name            = azurerm_shared_image_gallery.packer.name
+  resource_group_name     = data.azurerm_resource_group.packer.name
+  location                = data.azurerm_resource_group.packer.location
+  os_type                 = each.value.os_type
+  hyper_v_generation      = each.value.generation
+  trusted_launch_enabled  = each.value.trusted_launch_enabled
+  confidential_vm_enabled = each.value.confidential_vm_enabled
 
   identifier {
     publisher = var.publisher
--- variables.tf
+++ variables.tf
@@ -7,12 +7,14 @@ variable "image_gallery_name" {
   type        = string
   description = "Name for the shared image gallery."
   default     = "packer_images"
+
   validation {
     condition     = length(var.image_gallery_name) >= 1 && length(var.image_gallery_name) <= 80
     error_message = "Image gallery names must be between 1 and 80 characters."
   }
+
   validation {
-    condition     = can(regex("[a-zA-Z0-9_\\.]+", var.image_gallery_name))
+    condition     = can(regex("^[a-zA-Z0-9_\\.]+$", var.image_gallery_name))
     error_message = "Image gallery names can only contain letters, numbers, underscores, and periods."
   }
 }
@@ -30,13 +32,30 @@ variable "publisher" {
 
 variable "image_definitions" {
   type = list(object({
-    name       = string
-    os_type    = string
-    offer      = string
-    sku        = string
-    app        = string
-    generation = optional(string, "V2")
+    name                    = string
+    os_type                 = string
+    offer                   = string
+    sku                     = string
+    app                     = string
+    generation              = optional(string, "V2")
+    trusted_launch_enabled  = optional(bool, true)
+    confidential_vm_enabled = optional(bool)
   }))
+
   description = "VM image definitions to create in the gallery."
   default     = []
+
+  validation {
+    condition = alltrue([for o in var.image_definitions:
+      anytrue([o.trusted_launch_enabled, o.confidential_vm_enabled]) && o.generation == "V2"
+    ])
+    error_message = "If trusted launch or confidential VM are enabled, `generation` must be `V2`."
+  }
+
+  validation {
+    condition = alltrue([for o in var.image_definitions:
+      ! alltrue([o.trusted_launch_enabled != null, o.confidential_vm_enabled != null])
+    ])
+    error_message = "Only one of `trusted_launch_enabled` or `confidential_vm_enabled` can be set."
+  }
 }
